46abbbda97baa07fdf153aa3e192b8df78ed2a9a
NiravGondalia <niravgondalia4599@gmail.com>
2025-12-04 18:33:52 +0530

    Create UI for add expense screen

---
+++ lib/feature/add_expense/presentation/widget/category_chip.dart
@@ -0,0 +1,38 @@
+import 'package:expense_tracker_app/core/extensions/size_extension.dart';
+import 'package:expense_tracker_app/core/helpers/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CategoryChip extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final bool isSelected;
+
+  const CategoryChip({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.isSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ChoiceChip(
+      label: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            size: 18,
+            color: isSelected ? Colors.white : AppColors.primary,
+          ),
+          4.hSpace,
+          Text(label),
+        ],
+      ),
+      selected: isSelected,
+      selectedColor: AppColors.primary,
+      labelStyle: TextStyle(color: isSelected ? Colors.white : Colors.black),
+      onSelected: (selected) {},
+    );
+  }
+}
